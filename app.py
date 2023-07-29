032266d936c20cb3c341cad4490eb8f9737bbcf7
SpicyTaco17 <114839175+SpicyTaco17@users.noreply.github.com>
2023-07-29 08:07:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 
 st.title('Solar Energy Forecasting')
 
-model = (model.json)
+model1 = (model.json)
 
 # DATE_COLUMN = 'date/time'
 # DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
