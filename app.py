b6728c9e7b259224ea0c768b41d15d11c2545b88
SpicyTaco17 <114839175+SpicyTaco17@users.noreply.github.com>
2023-07-29 08:17:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title('Solar Energy Forecasting')
 
 source = ('https://github.com/SpicyTaco17/Solar-Energy-Forecasting/blob/main/model.json')
 
-model = pd.read_json(source)
+model = json.load(source)
 
 # DATE_COLUMN = 'date/time'
 # DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
