d8f1a5ef136c6baa78e90c074f590c80b7dc996f
SpicyTaco17 <114839175+SpicyTaco17@users.noreply.github.com>
2023-07-29 21:30:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 import streamlit as st
 import pandas as pd
 import numpy as np
-import xgboost
+import xgboost as xgb
 from xgboost import XGBRegressor
 
 st.title('Solar Energy Forecasting')
